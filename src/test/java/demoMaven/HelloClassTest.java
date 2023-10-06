238648a20f07f698cd94f41874a9ba1eb0f96fec
NG <ng@gmail.com>
2023-10-06 15:03:52 +0200

    test

---
+++ src/test/java/demoMaven/HelloClassTest.java
@@ -0,0 +1,17 @@
+package demoMaven;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class HelloClassTest {
+
+	
+	@Test 
+	void testHello() {
+		
+		assertEquals(4,3,"Doit retourner la surface d'un carré.");
+	}
+
+
+}
